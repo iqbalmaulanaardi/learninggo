1dc7c6e61ce3387cbdc94a2c566e62cdc8e7d999
Iqbal Maulana <iqbal@kawancicil.co.id>
2019-08-15 11:16:18 +0700

    add gorilla mux

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"github.com/gorilla/mux"
 )
-func handlerFunc(w http.ResponseWriter, r *http.Request){
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+func homeHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "<h1>Welcome to my home</h1>")
+}
+func contactHandler(w http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "<h1>Welcome to my contact</h1>")
 }
 func main(){
-	// fmt.Println("Hello")
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", homeHandler)
+	r.HandleFunc("/contact", contactHandler)
+	http.ListenAndServe(":3000", r)
 }
